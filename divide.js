7dae60b66f3837d56cfa7917e7ad509df39489f6
Luan Gong <luan.gong@gmail.com>
2017-12-14 00:02:56 -0800

    Allow the dividend to be 0

--- divide.js
+++ divide.js
@@ -1,11 +1,8 @@
 'use strict';
 
-module.exports = function divide(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new TypeError('The arguments must be numbers');
+module.exports = function divide(divisor, dividend) {
+  if (typeof divisor !== 'number' || typeof dividend !== 'number') {
+    throw new TypeError('divide(): arguments must be numbers');
   }
-  if (b === 0) {
-    throw new Error('Divide by zero');
-  }
-  return a / b;
+  return divisor / dividend;
 };
--- divide.test.js
+++ divide.test.js
@@ -2,14 +2,30 @@
 
 const divide = require('./divide');
 
-it('divide(6, 3) should return 2', () => {
+it('divide(42, 2) should return 21', () => {
   expect(divide(6, 3)).toBe(2);
 });
 
-it('divide(6, 0) should throw an error', () => {
-  expect(() => divide(6, 0)).toThrow();
+it('divide(42, 0) should return Infinity', () => {
+  expect(divide(42, 0)).toBe(Infinity);
+});
+
+it('divide(-42, 0) should return -Infinity', () => {
+  expect(divide(-42, 0)).toBe(-Infinity);
+});
+
+it('divide(42, -0) should return -Infinity', () => {
+  expect(divide(42, -0)).toBe(-Infinity);
+});
+
+it('divide(-42, -0) should return Infinity', () => {
+  expect(divide(-42, -0)).toBe(Infinity);
+});
+
+it('divide(0, 0) should return NaN', () => {
+  expect(isNaN(divide(0, 0))).toBe(true);
 });
 
 it(`divide('foo', 3) should throw a TypeError`, () => {
-  expect(() => divide('foo', 0)).toThrow(TypeError);
+  expect(() => divide('foo', 3)).toThrow(TypeError);
 });
