9959389255217f9b6313ef226ec2f59ad88e3a2c
rajmohanpdy <raj4057@gmail.com>
2021-02-03 19:09:45 +0530

    How to use git cherry-pick

--- src/add.js
+++ src/add.js
@@ -1,8 +1,17 @@
 'use strict';
-
+// 
 module.exports = function add(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError('add(): arguments must be numbers');
   }
   return a + b;
 };
+
+// # Find and copy the commit hash that you want to send to another branch
+// git log
+
+// # change to the branch where this commit is needed
+// git checkout <branch-name>
+
+// # execute the cherry pick using the hash you copied earlier
+// git cherry-pick <commit-hash>
